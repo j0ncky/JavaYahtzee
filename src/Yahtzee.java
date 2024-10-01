3d08b7c3a492a461c94cbb777d1a94ef2df20015
j0ncky <josh.gibson700@gmail.com>
2024-09-30 21:51:52 -0500

    WIP

--- src/Yahtzee.java
+++ src/Yahtzee.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Yahtzee {
     public static void main(String[] args) {
@@ -42,7 +43,10 @@ public class Yahtzee {
     private static void ChooseDice() {
         Scanner scan = new Scanner(System.in);
         boolean illegal = true;
-        int[] diceChoices = new int[5];
+        boolean[] diceChoices = new boolean[5];
+        for (int i = 0; i < diceChoices.length; i++) {
+            diceChoices[i] = false;
+        }
         
         while (illegal) {
             illegal = false;
@@ -51,23 +55,31 @@ public class Yahtzee {
             String input = scan.nextLine();
             String[] tempArray = input.split(" ");
             for (int i = 0; i < tempArray.length; i++) {
-                diceChoices[i] = Integer.parseInt(tempArray[i]);
-                System.out.println(diceChoices[i]);
+                diceChoices[Integer.parseInt(tempArray[i]] = true;
+                
             }
+            for (int i = 0; i < diceChoices.size(); i++) {
+                if (diceChoices.get(i) == 0) {
+                    diceChoices.remove(i);
+                    System.out.println(diceChoices.get(i));
+                }
+            }
+            diceChoices.trimToSize();
+            
         
             //For loop for validating user input
-            for (int i = 0; i < diceChoices.length; i++) {
+            for (int i = 0; i < diceChoices.size(); i++) {
                 
                 //Checks if any numbers are the same
-                for (int j = diceChoices.length - 1; j > i; j--) {
-                    if (diceChoices[i] == diceChoices[j]) {
+                for (int j = diceChoices.size() - 1; j > i; j--) {
+                    if (diceChoices.get(i) == diceChoices.get(j)) {
                         illegal = true;
                         System.out.println("You cannot save the same die more than once.");
                     }
                 }
                 
                 //Checks if any integers are smaller than 1 or greater than 5
-                if (diceChoices[i] < 1 || diceChoices[i] > 5 ) {
+                if (diceChoices.get(i) < 1 || diceChoices.get(i) > 5 ) {
                     illegal = true;
                     System.out.println("Please only input numbers from 1 to 5.");
                     break;
@@ -76,7 +88,7 @@ public class Yahtzee {
             }
         }
 
-        for (int i = 0; i < diceChoices.length; i++) {
+        for (int i = 0; i < diceChoices.size(); i++) {
             System.out.println();
         }
 
