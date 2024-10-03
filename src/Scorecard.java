607dc55b6d01b23cbdad1c1af97a231e9077d0b7
j0ncky <josh.gibson700@gmail.com>
2024-10-03 00:24:03 -0500

    Last commit lied, this commit contains upper section scoring logic

--- src/Scorecard.java
+++ src/Scorecard.java
@@ -11,10 +11,72 @@ public class Scorecard {
 		Arrays.fill(possible, false);
 	}
 	
+	//13 different if statements, checking each scoring category with the dice array to see which ones are eligible to be scored
 	public void currentEligibleScores(Dice[] dice) {
-		for (int i = 0; i < possible.length; i++) {
-			System.out.println(possible[i]);
+		int aces, twos, threes, fours, fives, sixes, threeOfAKind, fourOfAKind, fullHouse, smallStraight, largeStraight, yahtzee, chance;
+		int arrayLength = dice.length;
+		
+		//Aces
+		if (dice[0].value == 1 || dice[1].value == 1 || dice[2].value == 1 || dice[3].value == 1 || dice[4].value == 1) {
+			aces = 0;
+			for (int i = 0; i < arrayLength; i++) {
+				if (dice[i].value == 1) {
+					aces++;
+				}
+			}
 		}
+		
+		//Twos
+		if (dice[0].value == 2 || dice[1].value == 2 || dice[2].value == 2 || dice[3].value == 2 || dice[4].value == 2) {
+			aces = 0;
+			for (int i = 0; i < arrayLength; i++) {
+				if (dice[i].value == 2) {
+					aces += 2;
+				}
+			}
+		}
+		
+		//Threes
+		if (dice[0].value == 3 || dice[1].value == 3 || dice[2].value == 3 || dice[3].value == 3 || dice[4].value == 3) {
+			aces = 0;
+			for (int i = 0; i < arrayLength; i++) {
+				if (dice[i].value == 3) {
+					aces += 3;
+				}
+			}
+		}
+		
+		//Fours
+		if (dice[0].value == 4 || dice[1].value == 4 || dice[2].value == 4 || dice[3].value == 4 || dice[4].value == 4) {
+			aces = 0;
+			for (int i = 0; i < arrayLength; i++) {
+				if (dice[i].value == 4) {
+					aces += 4;
+				}
+			}
+		}
+		
+		//Fives
+		if (dice[0].value == 5 || dice[1].value == 5 || dice[2].value == 5 || dice[3].value == 5 || dice[4].value == 5) {
+			aces = 0;
+			for (int i = 0; i < arrayLength; i++) {
+				if (dice[i].value == 5) {
+					aces += 5;
+				}
+			}
+		}
+		
+		//Sixes
+		if (dice[0].value == 6 || dice[1].value == 6 || dice[2].value == 6 || dice[3].value == 6 || dice[4].value == 6) {
+			aces = 0;
+			for (int i = 0; i < arrayLength; i++) {
+				if (dice[i].value == 6) {
+					aces += 6;
+				}
+			}
+		}
+		
+		
 	}
 	
 	public void displayCurrentScores() {
